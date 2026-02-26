6d80396579b7301c966e01b055c85125f3aa4d29
Hackpig1974 <me@damon1974.com>
2026-02-25 20:38:38 -0700

    Add settings panel and UX improvements\n

--- preload.js
+++ preload.js
@@ -27,5 +27,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
   // API
   fetchUsageData: () => ipcRenderer.invoke('fetch-usage-data'),
-  openExternal: (url) => ipcRenderer.send('open-external', url)
+  openExternal: (url) => ipcRenderer.send('open-external', url),
+
+  // Settings
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings)
 });
